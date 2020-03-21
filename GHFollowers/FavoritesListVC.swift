00ef64ebe651b989b5610914e80bb4bcb24f2a4e
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-21 16:47:40 -0400

    Initial Commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by jonathan ferrer on 3/21/20.
+//  Copyright © 2020 jonathan ferrer. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by jonathan ferrer on 3/21/20.
+//  Copyright © 2020 jonathan ferrer. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        
+    }
+
+
+}
+
