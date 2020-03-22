ac5a9885daab655ca1ee15410e413a935f08df58
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-21 20:10:18 -0400

    created follower and user models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by jonathan ferrer on 3/21/20.
+//  Copyright © 2020 jonathan ferrer. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
