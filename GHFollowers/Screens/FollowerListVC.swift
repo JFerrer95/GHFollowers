1ade4c3e0bc226b051ebfdb6431419b320a8cfa1
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-21 21:20:12 -0400

    network calls refactored for result type

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -16,6 +16,25 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+                
+            case .success(let followers):
+                print(followers.count)
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+            }
+            
+            
+            
+            
+            
+           
+            
+        }
     }
     
     
