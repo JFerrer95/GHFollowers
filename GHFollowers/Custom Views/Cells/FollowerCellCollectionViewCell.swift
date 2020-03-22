d79ba2a7d27b9dc8ff1d8a68245e6d2dc24cfbdc
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-21 21:34:50 -0400

    created custom imageView

---
+++ GHFollowers/Custom Views/Cells/FollowerCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FollowerCellCollectionViewCell.swift
+//  GHFollowers
+//
+//  Created by jonathan ferrer on 3/21/20.
+//  Copyright © 2020 jonathan ferrer. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCellCollectionViewCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    let avitarImageView = UIImageView()
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+}
