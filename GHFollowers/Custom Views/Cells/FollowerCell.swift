6f448c70ea28e6891243e346ec0589388eac0c18
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-21 21:57:31 -0400

    collectionView initial set up

---
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,50 @@
+//
+//  FollowerCellCollectionViewCell.swift
+//  GHFollowers
+//
+//  Created by jonathan ferrer on 3/21/20.
+//  Copyright © 2020 jonathan ferrer. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    let avitarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avitarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avitarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avitarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avitarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: padding),
+            avitarImageView.heightAnchor.constraint(equalTo: avitarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avitarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+                                                   
+            
+            ])
+        
+    }
+}
